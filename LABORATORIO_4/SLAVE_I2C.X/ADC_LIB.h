fbd4dc7ced6d6dc5ede4425ade990ff8800da2fb
GAFong <50242187+GAFong@users.noreply.github.com>
2021-08-05 00:11:16 -0600

    LABORATORIO4
    PRELAB HECHO

---
+++ LABORATORIO_4/SLAVE_I2C.X/ADC_LIB.h
@@ -0,0 +1,50 @@
+/* 
+ * File:   ADC_LIB.h
+ * Author: DELL
+ *
+ * Created on July 26, 2021, 6:38 PM
+ */
+
+#ifndef ADC_LIB_H
+#define	ADC_LIB_H
+#define _XTAL_FREQ 4000000
+
+void ADC_INIT(unsigned int canal){
+    //CONFIGURACION DEL ADC
+    ADCON0bits.ADON = 0X01;     //ENCENDEMOS EL MODULO
+    ADCON0bits.CHS = canal;      //ESCOGEMOS EL CANAL 0
+    __delay_us(100);
+    ADCON0bits.ADCS = 0X01;     //ADC CLOCK FOSC/8  
+    ADCON1bits.ADFM = 0;        //JUSTIFICADO A LA IZQUIERDA
+    ADCON1bits.VCFG0 = 0;       //VOLTAGE DE REFERENCIA EN VDD
+    ADCON1bits.VCFG1 = 0;       //VOLTAGE DE REFERENCIA EN VSS
+    PIE1bits.ADIE = 1;          //HABILITAMOS LA INTERRUPCION DEL ADC
+    PIR1bits.ADIF = 0;
+}
+unsigned int ADC_READ(void){
+    unsigned int LECT;
+    LECT = ADRESH;
+    return LECT;
+}
+void ADC_CHANNELS(unsigned int CANAL, unsigned int VALOR_ADC, unsigned int* VALORA){
+    //CANAL AL QUE QUEREMOS CAMBIAR, LECTURA DEL ADC, VARIABLE 1, VARIABLE 2,ADCON0bits.CHS 
+    switch (ADCON0bits.CHS){
+        case 0:
+            *VALORA = VALOR_ADC;
+            ADCON0bits.CHS = CANAL;
+            __delay_us(50);
+            ADCON0bits.GO = 1;
+            break;
+        case 1:
+            *VALORA = VALOR_ADC;
+            ADCON0bits.CHS = CANAL;
+            __delay_us(50);
+            ADCON0bits.GO = 1;
+            break;
+    }
+    
+    
+}
+
+#endif	/* ADC_LIB_H */
+
